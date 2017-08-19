bc9fc7d1b4ae4ea4f7075b79f77c44ddeabf6c0e
chenxi <bbxycx.18@163.com>
2017-08-19 15:27:14 +0800

    测试40000*40000遍历相加go的性能 Please enter the commit message for your changes. Lines starting

---
+++ test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// fmt.Println("hello world")
+	r := RandInt(10, 13)
+	fmt.Println(r, time.Now().UnixNano())
+	t := time.Now().UnixNano()
+	sum := 0
+	for i := 0; i <= 40000+r; i++ {
+		for j := 0; j <= 40000; j++ {
+			sum = sum + i*j
+		}
+	}
+	i := float64(time.Now().UnixNano()-t) / (1000000 * 1000)
+	fmt.Println(sum)
+	fmt.Println(i)
+}
+
+func RandInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
