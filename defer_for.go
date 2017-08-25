5816828636e26f9cd86986c2b6596c69222ae3e2
chenxi2015 <bbxycx.18@163.com>
2017-08-25 14:47:50 +0800

    延迟的函数调用被压入一个栈中。当函数返回时， 会按照后进先出的顺序调用被延迟的函数调用。

---
+++ defer_for.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	
+	fmt.Println("counting")
+	
+	count := 10
+	for i := 0; i < count; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
